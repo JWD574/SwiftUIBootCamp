8861eb6938089d57c719aacec352588195e6e1cf
rangerboy574 <jdeweese574@gmail.com>
2022-12-14 15:37:47 -0600

    Initial Commit

---
+++ SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampApp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Joseph  DeWeese on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
